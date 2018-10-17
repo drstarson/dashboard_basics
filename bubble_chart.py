e33ea62fb1003567890d0522d9ef526a23e3bd0a
drstarson <dimlyus@gmail.com>
2018-10-16 20:58:20 -0700

    adds bubble chart py file and mpg.csv

---
+++ bubble_chart.py
@@ -0,0 +1,20 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('data/mpg.csv')
+#print (df[['mpg', 'name']].head())
+
+data = [go.Scatter(x=df['horsepower'],
+                   y=df['mpg'],
+                   text=df['name'],
+                   mode='markers',
+                   marker=dict(size=2*df['cylinders'],
+                               color=df['weight'],
+                               showscale=True))]
+
+
+layout = go.Layout(title='Bubble Chart')
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig)
